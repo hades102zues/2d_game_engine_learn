39cd9917d237795096f775a98f9638a151a9e224
hades102zues <joshua.kar@hotmail.com>
2022-09-07 22:09:17 -0400

    adds delta time

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,30 @@ int main(int argc, char** argv) {
 
 	Window* window = new Window("GAME");
 
+
+    float currentFrameTime;
+    float lastFrameTime;
+    float dt;
+
+    // Note that gabe is doing time relative to when the application starts
+    // Just a thought-- should things need refactoring later
+    lastFrameTime = (float) glfwGetTime();
+
 	while (!glfwWindowShouldClose(window->glWindow))
     {
+        /* Poll for and process events */
+        glfwPollEvents();
+
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window->glWindow);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+
+        currentFrameTime = (float) glfwGetTime(); 
+        dt = currentFrameTime - lastFrameTime;
+        lastFrameTime = currentFrameTime;
     }
 	delete window;
 	return 0;
